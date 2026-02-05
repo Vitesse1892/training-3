5947f83f08de3dc3a6fbe4bdb5b1fda6377d3a31
leroy <leroy.vanomme@gmail.com>
2026-02-05 17:06:58 +0100

    20260205_PushSoFar

--- frontend/TA_leroy/pageObjects/ShadowDomPage.js
+++ frontend/TA_leroy/pageObjects/ShadowDomPage.js
@@ -1,14 +1,70 @@
+import { expect } from "@playwright/test";
+
 export class ShadowDomPage {
   /**
    * @param {import('@playwright/test').Page} page
    */
-    
-    constructor(page) {
-        this.page = page;
 
-        //Elements
+  constructor(page) {
+    this.page = page;
 
+    //Elements
+    this.btnClickMe = page.getByTestId("shadow-button");
+    this.txtStatusClickCount = page.getByTestId("shadow-status");
+
+    this.fieldEnterText = page.getByTestId("shadow-input");
+    this.btnSubmit = page.getByTestId("shadow-submit");
+    this.txtResultSubmitted = page.getByTestId("shadow-result");
+  }
+
+  //Actions
+  async goToShadowDomPage() {
+    await this.page.goto("/shadow-dom");
+  }
+
+  async assertClickStatusTextVisible() {
+    await expect(this.txtStatusClickCount).toBeVisible();
+  }
+
+  async assertSubmitResultTextVisible() {
+    await expect(this.txtResultSubmitted).toBeVisible();
+  }
+
+  async assertClickStatusTextHidden() {
+    await expect(this.txtStatusClickCount).toBeHidden();
+  }
+
+  async assertSubmitResultTextHidden() {
+    await expect(this.txtResultSubmitted).toBeHidden();
+  }
+
+  async clickClickMeButton(amountOfClicks) {
+    for (let i = 0; i < amountOfClicks; i++) {
+      await this.btnClickMe.click();
     }
+  }
 
-    //Actions
+  async assertClickCountTextToBeEmpty() {
+    await expect(this.txtStatusClickCount).toHaveText("");
+  }
+
+  async assertSubmitTextToBeEmpty() {
+    await expect(this.txtResultSubmitted).toHaveText("");
+  }
+
+  async assertClickCountText(expectedCount) {
+    const suffix = expectedCount === 1 ? "time" : "times";
+    await expect(this.txtStatusClickCount).toHaveText(
+      `Clicked ${expectedCount} ${suffix}`,
+    );
+  }
+
+  async enterTextAndSubmit(text) {
+    await this.fieldEnterText.fill(text);
+    await this.btnSubmit.click();
+  }
+
+  async assertSubmitText(text) {
+    await expect(this.txtResultSubmitted).toHaveText(`Submitted: "${text}"`);
+  }
 }
---
+++ frontend/TA_leroy/tests/shadowDom.spec.js
@@ -0,0 +1,65 @@
+import { log } from 'node:console';
+import { test, expect } from '../fixtures/pages.js';
+
+//Verder kijken naar methodes, aangezien genoemde elementen altijd aanwezig zijn.
+
+test('1. De click me status tekst is bij aanvang niet zichtbaar', async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.assertClickCountTextToBeEmpty();
+});
+
+test("2. De submit result tekst is bij aanvang leeg, element wel zichtbaar", async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.assertSubmitResultTextVisible();
+  await shadowDomPage.assertSubmitTextToBeEmpty();
+});
+
+test('3. Na 1 klik op de click me button is de click me status tekst zichtbaar en toont deze dat er 1 klik is geweest', async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.assertClickCountTextToBeEmpty();
+  await shadowDomPage.clickClickMeButton(1);
+  await shadowDomPage.assertClickStatusTextVisible();
+  await shadowDomPage.assertClickCountText(1);
+});
+
+test('4. Na 5 klikken op de click me button is de click me status tekst zichtbaar en toont deze dat er 5 klikken zijn geweest', async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.clickClickMeButton(5);
+  await shadowDomPage.assertClickCountText(5);
+});
+
+test('5. Na het invullen van tekst in het input veld en klikken op submit, is de submit result tekst zichtbaar en toont deze de ingevulde tekst', async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.assertSubmitTextToBeEmpty();
+  const textToSubmit = "This is the first text to submit!";
+  await shadowDomPage.enterTextAndSubmit(textToSubmit);
+  await shadowDomPage.assertSubmitText(textToSubmit);
+});
+
+test('6. Na het invullen van een andere tekst in het input veld en klikken op submit, is de submit result tekst zichtbaar en toont deze de nieuwe ingevulde tekst', async ({ shadowDomPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  const firstTextToSubmit = "This is the first text to submit!";
+  const secondTextToSubmit = "Another text to submit!";
+  await shadowDomPage.enterTextAndSubmit(firstTextToSubmit);
+  await shadowDomPage.assertSubmitText(firstTextToSubmit);
+  await shadowDomPage.enterTextAndSubmit(secondTextToSubmit);
+  await shadowDomPage.assertSubmitText(secondTextToSubmit);
+});
+
+test("7. De getoonde Click Me tekst en submit tekst verdwijnen na verlaten shadow-dom page en terugkeren", async ({ shadowDomPage, loginPage }) => {
+  await shadowDomPage.goToShadowDomPage();
+  const textToSubmit = "This is the first text to submit!......";
+  await shadowDomPage.enterTextAndSubmit(textToSubmit);
+  await shadowDomPage.assertSubmitText(textToSubmit);
+  await shadowDomPage.clickClickMeButton(10);
+  await shadowDomPage.assertClickCountText(10);
+
+  //Verlaat de pagina en keer terug. Click Me tekst en submit tekst zijn verdwenen
+  await loginPage.goToLoginPage();
+  await shadowDomPage.goToShadowDomPage();
+  await shadowDomPage.assertClickCountTextToBeEmpty();
+  await shadowDomPage.assertSubmitTextToBeEmpty();
+});
+
+
+
